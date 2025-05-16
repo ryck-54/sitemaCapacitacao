64d74da952e1b85baf92fa8de1d48c698ba40624
Ryck <matheusryckson2@gmail.com>
2025-05-16 10:34:57 -0300

    Atualização

---
+++ sistemaCapacitacao-main/src/AlgoritimoMedia.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AlgoritimoMedia {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double n1, n2, n3, n4;
+        System.out.println(" Entre com primeira nota ");
+        n1 = entrada.nextDouble();
+        System.out.println(" Entre com segunda nota ");
+        n2 = entrada.nextDouble();
+        System.out.println(" Entre com terceira nota ");
+        n3 = entrada.nextDouble();
+        System.out.println(" Entre com quarta nota ");
+        n4 = entrada.nextDouble();
+        double media = (n1+n2+n3+n4) /4;
+        System.out.println("A media do discrente foi " + media);
+        if (media <5){
+            System.out.println(" aluno reprovado ");
